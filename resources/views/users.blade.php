9d18e19ab92ba7a445ec676cb0c7a117b478ec92
Kasun Dissanayake <kasunprageethdissanayake@gmail.com>
2018-09-07 23:22:31 +0530

    done

---
+++ resources/views/users.blade.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Users Page</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <h2>Users Page here .Please put your contributoion here</h2>
+    <p>My username is {{  $username }}</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('users', ['uses'=>'UserController@index']);
